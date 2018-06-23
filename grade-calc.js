fde56af7ecfe323900bb6ca7d43ca61d2a68e2f4
Oscar <strange2155@yahoo.com>
2018-06-22 21:25:43 -0500

    added file grade-calc

---
+++ grade-calc.js
@@ -0,0 +1,26 @@
+// students score, total possible score
+// 15/20 -> You got a C (75%)!
+// A 90- 100, B 80-90, C 70-79, D 60-69 F 0-59
+
+var score = 20
+
+var A = 'A';
+var B = 'B'
+var C = 'C'
+var D = 'D'
+var F = 'F'
+
+let gradeCalc = function(num){
+
+    return score - num
+
+
+//grade
+};
+
+//possible score
+
+
+//total possible score
+var studentScore = gradeCalc(18)
+console.log(studentScore)
